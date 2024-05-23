6d6c5bd524f3f3868a6032f60cdbb3e59a8adfa2
madebyjun <jun.m.3839@gmail.com>
2024-05-23 10:51:46 +0900

    ああ

--- create_poll.py
+++ create_poll.py
@@ -1,55 +1,48 @@
 import os
-import discord
-from discord.ext import commands
+import requests
 from datetime import datetime, timedelta
 
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-GUILD_ID = os.getenv('DISCORD_GUILD_ID')
 CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
 
-intents = discord.Intents.default()
-intents.message_content = True  # メッセージ内容の取得を有効にする
-bot = commands.Bot(command_prefix='!', intents=intents)
+# Poll Create Request Object
+poll_question = {
+    'text': "What's your favorite color?",
+}
+poll_answers = [
+    {'answer_id': 1, 'poll_media': {'text': 'Red', 'emoji': '🔴'}},
+    {'answer_id': 2, 'poll_media': {'text': 'Blue', 'emoji': '🔵'}},
+    {'answer_id': 3, 'poll_media': {'text': 'Green', 'emoji': '🟢'}}
+]
+duration_hours = 24  # Poll duration in hours
+allow_multiselect = False
+layout_type = 1
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user}')
-    guild = discord.utils.get(bot.guilds, id=int(GUILD_ID))
-    if guild is None:
-        print(f'Guild with ID {GUILD_ID} not found.')
-        await bot.close()
-        return
+# Expiry time
+expiry_time = (datetime.utcnow() + timedelta(hours=duration_hours)).isoformat()
 
-    channel = discord.utils.get(guild.text_channels, id=int(CHANNEL_ID))
-    if channel is None:
-        print(f'Channel with ID {CHANNEL_ID} not found.')
-        await bot.close()
-        return
+poll_request = {
+    'question': poll_question,
+    'answers': poll_answers,
+    'expiry': expiry_time,
+    'allow_multiselect': allow_multiselect,
+    'layout_type': layout_type,
+}
 
-    # Poll Create Request Object
-    poll_question = {
-        'text': "What's your favorite color?",
-    }
-    poll_answers = [
-        {'answer_id': 1, 'poll_media': {'text': 'Red', 'emoji': '🔴'}},
-        {'answer_id': 2, 'poll_media': {'text': 'Blue', 'emoji': '🔵'}},
-        {'answer_id': 3, 'poll_media': {'text': 'Green', 'emoji': '🟢'}}
-    ]
-    duration_hours = 24  # Poll duration in hours
-    allow_multiselect = False
-    layout_type = 1
+headers = {
+    'Authorization': f'Bot {TOKEN}',
+    'Content-Type': 'application/json'
+}
 
-    # Expiry time
-    expiry_time = datetime.utcnow() + timedelta(hours=duration_hours)
+data = {
+    'poll': poll_request,
+    'content': f"**{poll_question['text']}**\n" +
+               '\n'.join([f"{answer['poll_media']['emoji']} {answer['poll_media']['text']}" for answer in poll_answers])
+}
 
-    poll_message = f"**{poll_question['text']}**\n"
-    for answer in poll_answers:
-        poll_message += f"{answer['poll_media']['emoji']} {answer['poll_media']['text']}\n"
+response = requests.post(f'https://discord.com/api/v9/channels/{CHANNEL_ID}/messages', json=data, headers=headers)
 
-    message = await channel.send(poll_message)
-    for answer in poll_answers:
-        await message.add_reaction(answer['poll_media']['emoji'])
-
-    await bot.close()
-
-bot.run(TOKEN)
+if response.status_code == 200:
+    print('Poll created successfully')
+else:
+    print(f'Failed to create poll: {response.status_code}, {response.text}')
